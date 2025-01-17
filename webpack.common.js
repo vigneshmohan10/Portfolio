4c0af104730dc03df906802d82245e913a6414ec
vignesh mohan <ishwaranvigneshm@hotmail.com>
2025-01-17 16:14:01 +0530

    Webpack dev & Prod Basic configs are done

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "my-first-webpack.bundle.js",
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "public/index.html",
